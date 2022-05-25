0e007d80a3912a6d4283f9181c1714271a0648c5
丘惟夫 <waveciou@gmail.com>
2022-05-25 18:53:09 +0800

    generate

---
+++ dist/_nuxt/1704146.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{358:function(n,e,t){"use strict";t.r(e);var r={asyncData:function(n){n.route,n.query;return(0,n.redirect)("/")}},c=t(63),component=Object(c.a)(r,undefined,undefined,!1,null,null,null);e.default=component.exports}}]);
